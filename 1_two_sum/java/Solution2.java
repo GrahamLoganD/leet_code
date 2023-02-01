18c66af3d1a683f4e6e0c757bd2ac9204aaf6250
Logan Graham <LDGBK6@mail.missouri.edu>
2023-01-31 20:36:04 -0600

    working on binary search solution

--- 1_two_sum/java/Solution2.java
+++ 1_two_sum/java/Solution2.java
@@ -1,23 +1,64 @@
-import java.util.Arrays;
-
 class Solution {
-    private int[] quickSort(int[] array) {
-        int partition_index;
-        int pivot_value = array[array.length - 1];
-        int temporary_pivot_index = 
-    }
-
-    private class binarySearchOutput {
-        private int middle_search_index;
-        private boolean successful;
-
-        private binarySearchOutput(int middle_search_index, boolean successful) {
-            this.middle_search_index = middle_search_index;
-            this.successful = successful;
+    private void quickSort(int[] array, int lower_index, int upper_index) {
+        if (lower_index >= upper_index || lower_index < 0) {
+            return;
         }
 
-        private int getMiddleSearchIndex() {
-            return middle_search_index;
+        int partition_index = partition(array, lower_index, upper_index);
+
+        quickSort(array, lower_index, partition_index - 1);
+        quickSort(array, partition_index + 1, upper_index);
+    }
+
+    private int partition(int[] array, int lower_index, int upper_index) {
+        int pivot_value = array[upper_index];
+        int temporary_pivot_index = lower_index - 1;
+
+        for (int i = lower_index; i < upper_index; i++) {
+            if (array[i] <= pivot_value) {
+                temporary_pivot_index++;
+                swap(array, temporary_pivot_index, i);
+            }
+        }
+
+        temporary_pivot_index++;
+        swap(array, temporary_pivot_index, upper_index);
+        return temporary_pivot_index;
+    }
+
+    private void swap(int[] array, int index_1, int index_2) {
+        int save_value = array[index_1];
+        array[index_1] = array[index_2];
+        array[index_2] = save_value;
+    }
+
+    private class BinarySearch {
+        private int target_index;
+        private boolean successful;
+
+        private BinarySearch(int[] array, int lower_index, int upper_index, int target_value) {
+            while (lower_index <= upper_index) {
+                int middle_index = (lower_index + upper_index) / 2;
+                int middle_value = array[middle_index];
+
+                if (middle_value < target_value) {
+                    lower_index = middle_index + 1;
+                }
+
+                else if (middle_value > target_value) {
+                    upper_index = middle_index - 1;
+                }
+
+                else {
+                    this.target_index = middle_index;
+                    this.successful = true;
+                    return;
+                }
+            }
+        }
+
+        private int getTargetIndex() {
+            return target_index;
         }
 
         private boolean isSuccessful() {
@@ -25,40 +66,21 @@ class Solution {
         }
     }
 
-    private binarySearchOutput binarySearch(int[] array, int target_value) {
-        int lower_search_index = 0;
-        int upper_search_index = array.length - 1;
-        int search_target_value = target_value;
-
-        while (lower_search_index <= upper_search_index) {
-            int middle_search_index = (lower_search_index + upper_search_index) / 2;
-            int middle_value = array[middle_search_index];
-
-            if (middle_value < search_target_value) {
-                lower_search_index = middle_search_index + 1;
-            }
-
-            else if (middle_value > search_target_value) {
-                lower_search_index = middle_search_index - 1;
-            }
-
-            else {
-                return new binarySearchOutput(middle_search_index, true);
-            }
-        }
-        return new binarySearchOutput(0, false);
-    }
-
     public int[] twoSum(int[] nums, int target) {
         int length = nums.length;
 
-        // sort i guess
+        quickSort(nums, 0, length - 1);
+
+        for (int i : nums) {
+            System.out.printf("%d ", i);
+        }
 
         for (int first_number_index = 0; first_number_index < length - 1; first_number_index++) {
-            binarySearchOutput binary_search_output = binarySearch(
-                    Arrays.copyOfRange(nums, first_number_index + 1, length - 1), target - nums[first_number_index]);
-            if (binary_search_output.isSuccessful()) {
-                return new int[] { first_number_index, binary_search_output.getMiddleSearchIndex() };
+            BinarySearch binary_search = new BinarySearch(nums, first_number_index + 1, length - 1,
+                    target - nums[first_number_index]);
+
+            if (binary_search.isSuccessful()) {
+                return new int[] { first_number_index, binary_search.getTargetIndex() };
             }
         }
         return null;
