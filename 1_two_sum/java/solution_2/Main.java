01620fe37d8cec967a66f0f0d9c6337c55319eea
Logan Graham <LDGBK6@mail.missouri.edu>
2023-02-01 16:02:35 -0600

    Update Main.java

--- 1_two_sum/java/solution_2/Main.java
+++ 1_two_sum/java/solution_2/Main.java
@@ -3,8 +3,11 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Case 1");
+        int[] nums_1 = { 2, 7, 11, 15 };
+        int target_1 = 9;
         Solution solution_1 = new Solution();
-        System.out.println("Output:   " + Arrays.toString(solution_1.twoSum(new int[] { 2, 7, 11, 15 }, 9)));
+        int[] output_1 = solution_1.twoSum(nums_1, target_1);
+        System.out.println("Output:   " + Arrays.toString(output_1));
         System.out.println("Expected: [0, 1]");
         System.out.println();
 
