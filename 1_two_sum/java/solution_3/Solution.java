05afd0b324b7c2ce16207beb569eba6424fbc07b
Logan Graham <LDGBK6@mail.missouri.edu>
2023-02-02 15:32:43 -0600

    hashmap solution

--- 1_two_sum/java/solution_3/Solution.java
+++ 1_two_sum/java/solution_3/Solution.java
@@ -1,5 +1,19 @@
+import java.util.HashMap;
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> array = new HashMap<Integer, Integer>();
+
+        for (int number_2_index = 0; number_2_index < nums.length; number_2_index++) {
+            Integer number_1_index = array.get(target - nums[number_2_index]);
+
+            if (number_1_index != null) {
+                return new int[] { number_1_index.intValue(), number_2_index };
+            }
+
+            array.put(nums[number_2_index], number_2_index);
+        }
+
         return null;
     }
 }
